32f346cc9b475134dadcba652093b428deb24acf
paul <ppdore@gmail.com>
2025-01-28 04:38:36 -0500

    Try to see pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import HomePage from './components/HomePage';
-import PowerToBuild from './components/powers/build';
-import PowerToAutomate from './components/powers/automate';
-import PowerToCreate from './components/powers/create';
-import PowerToConnect from './components/powers/connect';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/powers/build" element={<PowerToBuild />} />
-        <Route path="/powers/automate" element={<PowerToAutomate />} />
-        <Route path="/powers/create" element={<PowerToCreate />} />
-        <Route path="/powers/connect" element={<PowerToConnect />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <HomePage />
+    </div>
   );
 }
 
